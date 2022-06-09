a92744ccacfc7289d26c1cc2a77b0ec44ffc050e
agray998 <a.gray.4797@gmail.com>
2022-06-09 09:46:46 +0100

    added test script

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+declare -a names=(Alice Bob Claire Derek)
+for name in "${names[@]}"; do
+  if [ $(java Greet $name) == "Hello, $name" ]; then
+    echo "Test Passed"
+  else
+    echo "Test failed!"
+  fi
+done
